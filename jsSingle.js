90c6a25a59f6a89c43e482b2949302cfdb7522f6
OgnenManchevski <46723553+OgMan1@users.noreply.github.com>
2020-08-03 17:03:14 +0200

    Add files via upload
    updated

--- jsSingle.js
+++ jsSingle.js
@@ -1,31 +1,27 @@
 function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
-    document.querySelector('#main').style.display = 'block';
-  }
-
-  function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-    document.querySelector('#main').style.display = 'none';
-  }
-
-
-  $(document).ready(function () {
-          $(window).scroll(function () {
-              var scroll = $(window).scrollTop();
-              if (scroll > 0) {
-                  $(".navTransparent").css("background", "rgba(63, 15, 117, 0.3)");
-              }
-
-              else {
-                  $(".navTransparent").css("background", " ");
-              }
-          })
-      })
-
-      $(document).keydown(function (e) {
-
-if (e.keyCode == 27) {
-document.getElementById("mySidenav").style.width = "0";
-document.querySelector('#main').style.display = 'none';
+  document.getElementById("mySidenav").style.width = "250px";
+  document.querySelector("#main").style.display = "block";
 }
-});     
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+  document.querySelector("#main").style.display = "none";
+}
+
+$(document).ready(function () {
+  $(window).scroll(function () {
+    var scroll = $(window).scrollTop();
+    if (scroll > 10) {
+      $(".navTransparent").css("background", "rgba(63, 15, 117, 0.85)");
+    } else {
+      $(".navTransparent").css("background", "rgba(63, 15, 117, 0.15)");
+    }
+  });
+});
+
+$(document).keydown(function (e) {
+  if (e.keyCode == 27) {
+    document.getElementById("mySidenav").style.width = "0";
+    document.querySelector("#main").style.display = "none";
+  }
+});
